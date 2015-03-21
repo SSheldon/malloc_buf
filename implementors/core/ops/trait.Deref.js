55e668ed36262ee9cacbe2d4c643d58806afc088
Steven Sheldon <steven@sasheldon.com>
2015-03-21 13:42:49 -0700

    Updated documentation.

--- implementors/core/ops/trait.Deref.js
+++ implementors/core/ops/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['malloc_buf'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='malloc_buf/struct.MallocBuffer.html' title='malloc_buf::MallocBuffer'>MallocBuffer</a>&lt;T&gt;","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='malloc_buf/struct.MallocString.html' title='malloc_buf::MallocString'>MallocString</a>",];
+implementors['malloc_buf'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='malloc_buf/struct.MallocBuffer.html' title='malloc_buf::MallocBuffer'>MallocBuffer</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);

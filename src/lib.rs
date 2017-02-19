e389c1ebb3a59c16940a1f5ea197e181b57ac1e3
Steven Sheldon <steven@sasheldon.com>
2017-02-19 09:59:12 -0800

    Replace MallocBuffer<T> with Malloc<[T]>

--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,52 @@
 extern crate libc;
 
-use std::marker::PhantomData;
 use std::ops::Deref;
 use std::slice;
 use libc::c_void;
 
-struct MallocPtr(*mut c_void);
-
-impl Drop for MallocPtr {
-    fn drop(&mut self) {
-        unsafe {
-            libc::free(self.0);
-        }
-    }
-}
+const DUMMY_PTR: *mut c_void = 0x1 as *mut c_void;
 
 /// A type that represents a `malloc`'d chunk of memory.
-pub struct MallocBuffer<T> {
-    ptr: MallocPtr,
-    len: usize,
-    items: PhantomData<[T]>,
+pub struct Malloc<T: ?Sized> {
+    ptr: *mut T,
 }
 
-impl<T: Copy> MallocBuffer<T> {
-    /// Constructs a new `MallocBuffer` for a `malloc`'d buffer
+impl<T: Copy> Malloc<[T]> {
+    /// Constructs a new `Malloc` for a `malloc`'d buffer
     /// with the given length at the given pointer.
     /// Returns `None` if the given pointer is null and the length is not 0.
     ///
-    /// When this `MallocBuffer` drops, the buffer will be `free`'d.
+    /// When this `Malloc` drops, the buffer will be `free`'d.
     ///
     /// Unsafe because there must be `len` contiguous, valid instances of `T`
     /// at `ptr`.
-    pub unsafe fn new(ptr: *mut T, len: usize) -> Option<MallocBuffer<T>> {
-        if len > 0 && ptr.is_null() {
-            None
-        } else {
-            Some(MallocBuffer {
-                ptr: MallocPtr(ptr as *mut c_void),
-                len: len,
-                items: PhantomData,
-            })
-        }
+    pub unsafe fn from_array(ptr: *mut T, len: usize) -> Option<Malloc<[T]>> {
+        let ptr = match (ptr.is_null(), len) {
+            // Even a 0-size slice cannot be null, so just use another pointer
+            (true, 0) => DUMMY_PTR as *mut T,
+            (true, _) => return None,
+            (false, _) => ptr,
+        };
+        let slice = slice::from_raw_parts(ptr, len);
+        Some(Malloc { ptr: slice as *const [T] as *mut [T] })
     }
 }
 
-impl<T> Deref for MallocBuffer<T> {
-    type Target = [T];
+impl<T: ?Sized> Deref for Malloc<T> {
+    type Target = T;
 
-    fn deref(&self) -> &[T] {
-        let ptr = if self.len == 0 && self.ptr.0.is_null() {
-            // Even a 0-size slice cannot be null, so just use another pointer
-            0x1 as *const T
-        } else {
-            self.ptr.0 as *const T
-        };
-        unsafe {
-            slice::from_raw_parts(ptr, self.len)
+    fn deref(&self) -> &T {
+        unsafe { &*self.ptr }
+    }
+}
+
+impl<T: ?Sized> Drop for Malloc<T> {
+    fn drop(&mut self) {
+        let ptr = self.ptr as *mut c_void;
+        if ptr != DUMMY_PTR {
+            unsafe {
+                libc::free(ptr);
+            }
         }
     }
 }
@@ -65,18 +56,18 @@ mod tests {
     use std::ptr;
     use libc;
 
-    use super::MallocBuffer;
+    use super::Malloc;
 
     #[test]
     fn test_null_buf() {
         let buf = unsafe {
-            MallocBuffer::<u32>::new(ptr::null_mut(), 0).unwrap()
+            Malloc::<[u32]>::from_array(ptr::null_mut(), 0).unwrap()
         };
         assert!(&*buf == []);
         assert!(Some(&*buf) == Some(&[]));
 
         let buf = unsafe {
-            MallocBuffer::<u32>::new(ptr::null_mut(), 7)
+            Malloc::<[u32]>::from_array(ptr::null_mut(), 7)
         };
         assert!(buf.is_none());
     }
@@ -88,7 +79,7 @@ mod tests {
             *ptr = 1;
             *ptr.offset(1) = 2;
             *ptr.offset(2) = 3;
-            MallocBuffer::new(ptr, 3).unwrap()
+            Malloc::from_array(ptr, 3).unwrap()
         };
         assert!(&*buf == [1, 2, 3]);
     }
